69e0b879a8dc8e469fbf19aa892c9dfeb0a410f7
Eduardo André Rodríguez Zelada <eduardo.rodriguezz@outlook.com>
2017-03-19 20:58:56 -0500

    add to contact function
    added to contact function.

--- vm/ServicesVm.js
+++ vm/ServicesVm.js
@@ -1,7 +1,7 @@
 var ServicesVm = {
     CurrentServices: ko.observableArray([]),
     Details: ko.observableArray([]),
-    LoadData: function(){
+    loadData: function(){
         var self = this;
         var services = [{Id: 0, Name: 'Cursos', Icon:'fa fa-graduation-cap'},
                         {Id: 1, Name:'Asesorías', Icon:'fa fa-comments-o'},
@@ -9,7 +9,7 @@ var ServicesVm = {
                         {Id: 3, Name:'Tramites', Icon:'fa fa-files-o'}]
         self.CurrentServices(services);
     },
-    ShowDetails: function(data, event){
+    showDetails: function(data, event){
         var self = ServicesVm;
         var details = [['Curso1', 'Curso2', 'Curso3'],['Asesoría Contable y Financiera', 'Asesoría en Gestión Pública'],['Item1', 'Item2', 'Item3'],['Item1','Item2']];
         $('.all-details').fadeOut('slow', function() { 
@@ -17,8 +17,11 @@ var ServicesVm = {
             $('.all-details').fadeIn(); 
         });
     },
+    toContact: function(data, event){
+        $('.content').load('./views/contact.html');
+    },
     init: function(){
-        ServicesVm.LoadData();
+        ServicesVm.loadData();
         ko.applyBindings(ServicesVm, $('.serv-data')[0]);
         $('div[data-type="1"]').click();
     }
